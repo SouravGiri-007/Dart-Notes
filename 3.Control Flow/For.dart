d9052416c0a4ad854e4deecf83055da92827ae96
Sourav <souravgiri247@gmail.com>
2025-02-11 23:13:48 +0530

    Control Flow

---
+++ 3.Control Flow/For.dart
@@ -0,0 +1,14 @@
+#for loop
+void main()
+{
+	for (int i = 0; i < 5; i++) {
+		print('Sourav ');
+	}
+}
+
+
+#For.Each Loop
+    The forEach loop in Dart is a convenient way to iterate over elements in a collection, such as a list, set, or map. 
+    It uses a callback function that applies an operation to each element
+
+    
---
+++ 3.Control Flow/Switch.dart
@@ -0,0 +1,38 @@
+#Syntex: 
+  void main() {
+  var choice = 2;
+
+  switch (choice) {
+    case 1:
+      print('You chose option 1');
+      break;
+    case 2:
+      print('You chose option 2');
+      break;
+    case 3:
+      print('You chose option 3');
+      break;
+    default:
+      print('Invalid choice');
+  }
+}
+
+
+#example: 
+   void main() {
+  var fruit = "Apple";
+
+  switch (fruit) {
+    case "Apple":
+      print("You chose Apple.");
+      break;
+    case "Banana":
+      print("You chose Banana.");
+      break;
+    case "Orange":
+      print("You chose Orange.");
+      break;
+    default:
+      print("Invalid fruit.");
+  }
+}
